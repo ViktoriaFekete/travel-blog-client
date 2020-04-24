ee035e1522c90160f7945cee6e5f7f796e8bbd05
unknown <jakub.marincic@gmail.com>
2020-04-24 02:08:54 +0200

    HappyCase for SignUP done

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import MainFrame from './navigation/MainFrame';
+import Registration from './screens/Registration';
 
 export default class App extends React.Component {
   render() {
     return (
-      <MainFrame/>
+      <Registration/>
     );
   }
 }
