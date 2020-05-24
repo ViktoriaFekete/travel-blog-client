34eb6708f16a7b72232054ed9801166106ae7e53
ViktoriaFekete <viktoria.fekete97@gmail.com>
2020-05-24 19:22:57 +0200

    Notification about being offline added to WelcomePage

--- screens/WelcomePage.js
+++ screens/WelcomePage.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-} from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, Overlay, Image, Icon } from 'react-native-elements';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 
@@ -11,6 +8,38 @@ export default class WelcomePage extends React.Component {
     title: 'Ahoj!'
   };
   
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      visible: false,
+      status: false,
+    }
+}
+
+  toggleOverlay() {
+    if (this.state.visible == true)
+      this.setState({visible: false});
+    else
+      this.setState({visible: true});
+  };
+
+  checkStateToRegistrate(){
+    const { navigate } = this.props.navigation
+    if (this.state.status == true)
+      navigate('Registration')
+    else
+      this.setState({visible: true});
+  }
+
+  checkStateToLogin(){
+    const { navigate } = this.props.navigation
+    if (this.state.status == true)
+      navigate('Login')
+    else
+      this.setState({visible: true});
+  }
+
   render() {
     const { navigate } = this.props.navigation
 
@@ -24,14 +53,14 @@ export default class WelcomePage extends React.Component {
                   type='solid' 
                   containerStyle={styles.buttons} 
                   buttonStyle={styles.btn}
-                  onPress = { () => navigate('Registration')}
+                  onPress = { () => this.checkStateToRegistrate()}
               />
               <Button 
                   title='Prihlás sa' 
                   type='solid' 
                   containerStyle={styles.buttons} 
                   buttonStyle={styles.btn}
-                  onPress = { () => navigate('Login') }
+                  onPress = { () => this.checkStateToLogin()}
               />
               <Button 
                   title='Pokračuj bez prihlásenia' 
@@ -40,6 +69,26 @@ export default class WelcomePage extends React.Component {
                   buttonStyle={styles.btn}
                   onPress={ () => navigate('Home') }
               />
+          </View>
+          <View>
+            <Overlay overlayStyle={styles.offlineMsg} isVisible={this.state.visible} onBackdropPress={this.toggleOverlay.bind(this)}>
+              <Icon
+                name='close'
+                type='ionicons'
+                color='#303133'
+                onPress = {this.toggleOverlay.bind(this)}
+                containerStyle={{position: 'absolute', top: 10, right: 10}}
+                size = {30}
+              />
+              <Text style={styles.msg}>Ooops, Si offline.</Text>
+              <Image
+                source={{ uri: "https://cdn.iconscout.com/icon/free/png-512/wifi-off-1768029-1502203.png" }}
+                opacity = {0.6}
+                style={{ width: 100, height: 100 }}
+              />
+              <Text style={styles.msg}>Skontroluj pripojenie na Internet a skús znova.</Text>
+              <Button title="Skúsiť znova" onPress = { () => console.log("get state function") } containerStyle={styles.tryagainBtn} buttonStyle={{ height: 50}}/>
+            </Overlay>
           </View>
       </View>
     );
@@ -68,6 +117,21 @@ const styles = StyleSheet.create({
   },
   btn:{
       height: 70,
+  },
+  offlineMsg:{
+    width: '85%',
+    justifyContent: 'center',
+    alignItems: 'center',   
+    alignSelf: 'center',
+  },
+  msg:{
+    fontSize: 24,
+    textAlign: 'center',
+    padding: 25,
+  },
+  tryagainBtn:{
+    paddingTop: 40,
+    width: '60%',
   }
 });
 
